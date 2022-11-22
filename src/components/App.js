724785cb58deebbf5f2bf4e74e6a0fc74c287635
Abhishek Raj Gupta <110536209+abhishekraj00@users.noreply.github.com>
2022-11-22 11:01:18 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,15 @@
-import React, { useReducer } from 'react'
-import { counterReducer } from '../reducers/counterReducer';
-import '../styles/App.css';
+import React, { useReducer } from "react";
+import { counterReducer } from "../reducers/counterReducer";
+import "../styles/App.css";
 const App = () => {
-const [state,dispatch] = useReducer(counterReducer,{/*initial state to be placed here*/})
+  const [state, dispatch] = useReducer(counterReducer, 0);
   return (
     <div id="main">
-
+      <span>{state}</span>
+      <button onClick={() => dispatch("inc")}>Add</button>
+      <button onClick={() => dispatch("dec")}>Sub</button>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
